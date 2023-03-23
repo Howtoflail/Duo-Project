419fd131e778e7f6692de261a08c6038476f9966
Dragos Andrei Munteanu <516586@student.fontys.nl>
2023-03-23 15:27:33 +0100

    Made gun feel like a bolt action rifle

--- Unity Project/Your Own Game/Assets/Scripts/Gun Scripts/Main Gun.cs
+++ Unity Project/Your Own Game/Assets/Scripts/Gun Scripts/Main Gun.cs
@@ -7,35 +7,106 @@ public class MainGun : MonoBehaviour
 {
     [SerializeField] private float damage = 10f;
     [SerializeField] private float range = 100f;
-    [SerializeField] private float fireRate = 15f;
+    [SerializeField] private float fireRate = 2.2f;
     [SerializeField] private float impactForce = 30f;
-    [SerializeField] private AudioClip shootClip;
+    [SerializeField] private AudioClip rifleShotClip;
+    [SerializeField] private AudioClip rifleCockClip;
+    [SerializeField] private AudioClip rifleReloadClip;
 
     public Camera fpsCam;
+    private AudioSource audioSource;
 
     private float nextTimeToFire = 0f;
+    private readonly float defaultMagazineSize = 5f;
+    private float currentMagazineAmmo = 0f;
+    private bool isReloading = false;
+    private bool isCocking = false;
+    private float shotTime = 0f;
+    private float waitTime = 3f;
 
     //Input - later
     //private bool fireButton = false;
     void Start()
     {
-        
+        audioSource = GetComponent<AudioSource>();
+        currentMagazineAmmo = defaultMagazineSize;
     }
 
     void Update()
     {
-        if(Input.GetKey(KeyCode.Mouse0) && Time.time >= nextTimeToFire)
+        //GetKeyDown is used because of the bolt action system
+        if(Input.GetKeyDown(KeyCode.Mouse0) && Time.time >= nextTimeToFire && currentMagazineAmmo > 0)
         {
-            nextTimeToFire = Time.time + 1f / fireRate;
+            nextTimeToFire = Time.time + fireRate;
             Shoot();
 
             //play audio
-            //audioSource.PlayOneShot(shootClip);
+            float currentAudioClipDuration = rifleShotClip.length;
+            audioSource.clip = rifleShotClip;
+            audioSource.Play();
+            Debug.Log(audioSource.clip.name);
+
+            //Cocking coroutine is not needed
+            /*if (audioSource.isPlaying == false && audioSource.clip == rifleCockClip && currentMagazineAmmo > 1f) 
+            {
+                Debug.Log("Current ammo: " + currentMagazineAmmo);
+                Cock();
+            } */ 
         }
+        else if(currentMagazineAmmo <= 0) 
+        {
+            //This is used to make the reload sound play after the last round is fired properly
+            if(Time.time - shotTime >= waitTime) 
+            {
+                Reload();
+            }
+        }
+    }
+
+    /*void Cock()
+    {
+        if(isCocking == false)
+        {
+            isCocking = true;
+            StartCoroutine(CockingCoroutine());
+        }
+    }
+
+    IEnumerator CockingCoroutine()
+    {
+        float cockingTime = rifleCockClip.length;
+
+        audioSource.PlayOneShot(rifleCockClip);
+        yield return new WaitForSeconds(cockingTime);
+        isCocking = false;
+    }*/
+
+    void Reload()
+    {
+        if(isReloading == false)
+        {
+            isReloading = true;
+            StartCoroutine(ReloadCoroutine());
+
+            //Cock();
+        }
+    }
+
+    IEnumerator ReloadCoroutine()
+    {
+        float reloadTime = rifleReloadClip.length;
+
+        audioSource.PlayOneShot(rifleReloadClip);
+        yield return new WaitForSeconds(reloadTime);
+        isReloading = false;
+        currentMagazineAmmo = defaultMagazineSize;
     }
 
     void Shoot()
     {
+        currentMagazineAmmo--;
+        shotTime = Time.time;
+
         RaycastHit hit;
         if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
         {
